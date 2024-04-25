2db1885eda2dd27ee63aa9b6291aacd002be07c5
renato <88345498+renatobicego@users.noreply.github.com>
2024-04-25 10:01:30 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Medialuna - Consultoría de impacto",
+    title: "Medialuna",
     description: "Medias deportivas y urbanas",
   },
   robots: {
